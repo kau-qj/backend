38bbc0336e7c45babbff4cf92087e653e5d04582
ParkJunGyu26 <junju404@naver.com>
2023-10-25 03:57:41 +0900

    gpt api call 오타 수정

--- src/middleware/gptMiddleware.js
+++ src/middleware/gptMiddleware.js
@@ -8,7 +8,7 @@ const runGPT35 = async function(prompt, courseInfo) {
     try{
         const completion = await openai.chat.completions.create({
             model: "gpt-3.5-turbo",
-            message: [
+            messages: [
                 { role: "system", content: "You are a software developer who recommends IT jobs to others."},
                 { role: "system", content: `${courseInfo}`},
                 { role: "user", content: "I am a college student majoring in software engineering. And I dream of becoming a backend developer. This semester, I am taking the course ‘Database Basics: Understanding and Using Database Basics’. Considering my professional interests, I applied a scale of 0 to 5 for the topic ‘Database Fundamentals’. Your score should say one thing. A score of 0 means a subject that does not need to be completed, and a score of 5 means a subject that must be completed. And tell me why you gave that score."},
@@ -16,7 +16,7 @@ const runGPT35 = async function(prompt, courseInfo) {
                 { role: "user", content: prompt }
             ]
         });
-        console.log(completion.data.choices[0].message.content);
+        console.log(completion.choices);
     } catch (error) {
         if (error instanceof OpenAI.APIError) {
             console.error(error.status);
