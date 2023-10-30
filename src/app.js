54e7047899cdde395660879e495cabc8e0368a65
Kimtae00 <117256782+Kimtae00@users.noreply.github.com>
2023-10-31 00:22:06 +0900

    진로사전 구조 추가

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ const port = process.env.PORT || 3000;
 const userRouter = require('./route/userRouter.js');
 const qjRouter = require('./route/qjRouter.js');
 const mypageRouter = require('./route/mypageRouter.js');
+const jobguideRouter = requie('/.route/jobguideRouter.js');
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +22,7 @@ app.use(cors());
 app.use('/', userRouter);
 app.use('/', qjRouter);
 app.use('/mypage', mypageRouter);
+app.use('/jobguide', jobguideRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
