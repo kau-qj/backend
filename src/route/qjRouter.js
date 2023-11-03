990ac8bb22cf73e3fcb72f1bf3103d40c24a5b73
ParkJunGyu26 <junju404@naver.com>
2023-11-04 05:42:18 +0900

    qj post->get으로 변경

--- src/route/qjRouter.js
+++ src/route/qjRouter.js
@@ -5,7 +5,7 @@ const qj = require('../controller/qjController');
 const gptMiddleware = require('../middleware/gptMiddleware');
 
 // 1. 입력 없이 내 정보 바탕으로 gpt call API
-router.post('/myJob', gptMiddleware, qj.postRecommend);
+router.get('/myJob', gptMiddleware, qj.getRecommend);
 
 // 2. 관심 직무 입력해서 gpt call API
 router.post('/newJob', gptMiddleware, qj.postInputJobRecommend);
