afaa32221ca4b24b914ee85965ff70873b1901ff
ParkJunGyu26 <junju404@naver.com>
2023-11-10 16:32:16 +0900

    .

--- src/app.js
+++ src/app.js
@@ -1,22 +1,27 @@
-// app.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const dotenv = require('dotenv');
 const { pool } = require('./config/database.js');
 const { logger } = require('./config/winston.js');
 const swaggerUi = require('swagger-ui-express');
 const secret = require('./config/secret.js')
 
 const app = express();
-const port = process.env.PORT || 3000;
-
-const userRouter = require('./route/userRouter.js')
+const port = secret.PORT;
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./config/swagger/swagger-output-localhost.json'), { explorer: true }));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(require('./config/swagger/swagger-output-server.json'), { explorer: true }));
+const userRouter = require('./route/userRouter.js');
+const qjRouter = require('./route/qjRouter.js');
+const mypageRouter = require('./route/mypageRouter.js');
+const homeRouter = require('./route/homeRouter.js');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', userRouter);
+app.use('/qj', qjRouter);
+app.use('/mypage', mypageRouter);
+app.use('/home', homeRouter);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
