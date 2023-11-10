03da08147ab8f2936de1778e52d4e94589fc2ca0
ParkJunGyu26 <junju404@naver.com>
2023-11-10 13:58:13 +0900

    크롤링 코드 수정

--- src/crawler/crawler-scheduler.js
+++ src/crawler/crawler-scheduler.js
@@ -1,7 +1,13 @@
 const cron = require('node-cron');
 const runCrawler = require('./crawler');
 
-cron.schedule('30 10,16 * * *', function() {
+// 크롤링 스케줄러를 매일 오전 10시 30분과 오후 4시 30분에 실행
+const job = cron.schedule('30 10,16 * * *', async function() {
     console.log('Cron job for crawler started at:', new Date());
-    runCrawler();
+    await runCrawler();
 });
+
+// 서버가 시작될 때 크롤링 스케줄러를 직접 실행
+if (job) {
+    job.start();
+}
