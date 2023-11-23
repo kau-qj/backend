6bed24c1795910bc50074247d48e9c43c072aaf4
ParkJunGyu26 <junju404@naver.com>
2023-11-23 23:59:43 +0900

    크롤링 수정 및 timeout 시간 연장

--- src/crawler/crawler-scheduler.js
+++ src/crawler/crawler-scheduler.js
@@ -1,18 +1,17 @@
 const schedule = require('node-schedule');
 const runCrawler = require('./recruit.crawler');
-const { pool } = require('../config/database'); // 데이터베이스 풀 임포트
+const { pool } = require('../config/database');
 
 // 매일 오전 10시 30분과 오후 4시 30분에 크롤러 실행
-const crawlerJob = schedule.scheduleJob('30 10,16 * * *', function() {
+const crawlerJob = schedule.scheduleJob('30 10,16 * * *', async function () {
     console.log('crawler start:', new Date());
-    runCrawler(pool) // 데이터베이스 연결 풀을 runCrawler에 전달
-        .then((result) => {
-            if (result === 'Duplicate data found') {
-                console.log('duplicate data:', new Date());
-                crawlerJob.cancel();
-            }
-        })
-        .catch((error) => {
-            console.error('error in crawler:', error);
-        });
+    try {
+        const result = await runCrawler(pool); // 데이터베이스 연결 풀을 runCrawler에 전달
+        if (result === 'Duplicate data found') {
+            console.log('duplicate data:', new Date());
+            crawlerJob.cancel();
+        }
+    } catch (error) {
+        console.error('error in crawler:', error);
+    }
 });
